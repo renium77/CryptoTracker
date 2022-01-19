c0066b08b69f10ecc45e151979275b8c97b561e4
renium77 <54940001+renium77@users.noreply.github.com>
2022-01-19 11:17:10 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ my-app/src/App.js
@@ -0,0 +1,58 @@
+import React, {useState, useEffect} from 'react';
+import Coin from './Coin';
+import axios from 'axios'
+import './App.css';
+
+function App() {
+
+      const [coins, setCoins]= useState([]);
+      const [search, setSearch]=useState('');
+
+      useEffect(() =>{
+        axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=100&page=1&sparkline=false').then(res=>{
+          setCoins(res.data);
+        }).catch(error => console.log(error))
+      },[]);
+
+      const handelChange = e => {
+        setSearch(e.target.value)
+      }
+
+      const filterCoins = coins.filter(coin => 
+        coin.name.toLowerCase().includes(search.toLowerCase())
+        )
+
+  return (
+    <div className="App">
+      <div className='coin-app'>
+      <div className='coin-search'>
+        <h1 className='coin-text'>Crypto Price Tracker</h1>
+        <form>
+          <input
+            className='coin-input'
+            type='text'
+            onChange={handelChange}
+            placeholder='Search for Coins ..'
+          />
+        </form>
+      </div>
+      {filterCoins.map(coin => {
+        return (
+          <Coin
+            key={coin.id}
+            name={coin.name}
+            price={coin.current_price}
+            symbol={coin.symbol}
+            marketcap={coin.total_volume}
+            volume={coin.market_cap}
+            image={coin.image}
+            priceChange={coin.price_change_percentage_24h}
+          />
+        );
+      })}
+    </div>
+    </div>
+  );
+}
+
+export default App;
